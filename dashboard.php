715e17c23f4b0b4cf8e23ae9043e47a49de5b8dd
TOXYIC-HACKY <seeoomummy5@gmail.com>
2025-08-11 14:36:18 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,14 @@
 <?php
+
 session_start();
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header("Location: ./login/");
     exit;
 }
+
+include './config.php';
+$query = new Database();
 
 $username = $_SESSION['username'];
 
