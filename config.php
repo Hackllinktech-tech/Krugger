9f6fe533fd23d21d461a6066b2f31a9c8b2aea1d
TOXYIC-HACKY <seeoomummy5@gmail.com>
2025-08-11 15:10:29 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "auth");
+define("DB_SERVER", "sql8.freesqldatabase.com");
+define("DB_USERNAME", "sql8794565");
+define("DB_PASSWORD", "xpw1fvexwS");
+define("DB_NAME", "sql8794565");
 
 class Database
 {
